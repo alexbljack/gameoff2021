4d3eb90c39b007336cfd6417e6d8231e1ac697db
alexbljack <alexbljack@gmail.com>
2021-11-22 22:56:20 +0300

    Fixed camera for 1, 2 levels

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,35 +1,41 @@
+using System;
 using UnityEngine;
+
+[Serializable]
+public struct AxisBounds
+{
+    public float min;
+    public float max;
+}
 
 public class CameraFollow : MonoBehaviour
 {
-    [SerializeField] GameObject toFollow;
+    [SerializeField] Transform toFollow;
     [SerializeField] float smoothTime;
     [SerializeField] bool freezeX;
     [SerializeField] bool freezeY;
 
-    [SerializeField] float minX;
-    [SerializeField] float maxX;
-    
-    Vector3 _velocity = Vector3.zero;
-    Camera _camera;
+    [SerializeField] AxisBounds xBounds;
+    [SerializeField] AxisBounds yBounds;
 
-    void Awake()
-    {
-        _camera = GetComponent<Camera>();
-    }
+    Vector3 _velocity = Vector3.zero;
 
     void LateUpdate()
     {
-        CenterCamera();
+        if (toFollow != null)
+        {
+            CenterCamera();
+        }
     }
     
     void CenterCamera()
     {
-        Vector3 targetPos = toFollow.transform.position;
+        Vector3 targetPos = toFollow.position;
         Vector3 currentPos = transform.position;
         var x = freezeX ? currentPos.x : targetPos.x;
         var y = freezeY ? currentPos.y : targetPos.y;
-        x = Mathf.Clamp(x, minX, maxX);
+        x = Mathf.Clamp(x, xBounds.min, xBounds.max);
+        y = Mathf.Clamp(y, yBounds.min, yBounds.max);
         var finalPos = new Vector3(x, y, currentPos.z);
         transform.position = Vector3.SmoothDamp(currentPos, finalPos, ref _velocity, smoothTime);
     }
